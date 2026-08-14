a925c7511c4c52757906d785b087dc2a8d9f851e
vyaptijagirdar <vyaptij@gmail.com>
2026-08-14 16:39:43 +0530

    Add files via upload

---
+++ server/middleware/admin.js
@@ -0,0 +1,16 @@
+export function adminOnly(req, res, next) {
+
+  if (!req.user) {
+    return res.status(401).json({
+      message: 'Authentication required'
+    });
+  }
+
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({
+      message: 'Admin access required'
+    });
+  }
+
+  next();
+}
